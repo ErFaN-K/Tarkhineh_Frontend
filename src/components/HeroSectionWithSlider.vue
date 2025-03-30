6307cbb9b8cb48f73a8fd1c18220281a5025b255
ErFaN-K <erfangamer138787@gmail.com>
2025-03-30 19:04:54 +0330

    Coding Hero Section Banner Component

--- src/components/HeroSectionWithSlider.vue
+++ src/components/HeroSectionWithSlider.vue
@@ -7,6 +7,9 @@ import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/pagination';
 
+// Component
+import HeroSectionBanner from './HeroSectionBanner.vue';
+
 </script>
 
 <template>
@@ -15,55 +18,16 @@ import 'swiper/css/pagination';
         <Swiper :modules="[Autoplay, Pagination, EffectFade]" :pagination="{ clickable: true, el: '.hero__page--swiper' }" :grabCursor="true" effect="fade" :autoplay="{ delay: 5000 }" :loop="true">
             <!-- Swiper Slide -->
             <SwiperSlide>
-                <div class="relative flex items-center justify-center bg-green-800 bg-hero bg-hero--1 h-44 sm:h-84">
-                    <!-- Title & Button -->
-                    <div class="flex flex-col items-center gap-y-4 md:gap-y-10 pt-5.25 md:pt-7">
-                        <h1 class="font-Morabba-Bold text-Tint-1 lg:text-[40px] text-base sm:text-2xl">تجربه
-                            غذای سالم و
-                            گیاهی به سبک
-                            ترخینه</h1>
-                        <button class="flex items-center h-6 px-2 text-sm text-white rounded-lg bg-Primary sm:px-4 lg:px-8 sm:h-8 lg:h-10 font-Dana sm:text-base lg:text-lg">
-                            <a href="#">سفارش آنلاین عذا</a>
-                        </button>
-                    </div>
-                </div>
+                <HeroSectionBanner bgNumber="1" content="تجربه غذای سالم و گیاهی به سبک ترخینه"/>
             </SwiperSlide>
             <SwiperSlide>
-                <div class="relative flex items-center justify-center bg-green-800 bg-hero bg-hero--2 h-44 sm:h-84">
-                    <!-- Title & Button -->
-                    <div class="flex flex-col items-center gap-y-4 md:gap-y-10 pt-5.25 md:pt-7">
-                        <h1 class="font-Morabba-Bold text-Tint-1 lg:text-[40px] text-base sm:text-2xl">تجربه
-                            فست فود
-                            های گیاهی</h1>
-                        <button class="flex items-center h-6 px-2 text-sm text-white rounded-lg bg-Primary sm:px-4 lg:px-8 sm:h-8 lg:h-10 font-Dana sm:text-base lg:text-lg">
-                            <a href="#">سفارش آنلاین عذا</a>
-                        </button>
-                    </div>
-                </div>
+                <HeroSectionBanner bgNumber="2" content="تجربه فست فود های گیاهی"/>
             </SwiperSlide>
             <SwiperSlide>
-                <div class="relative flex items-center justify-center bg-green-800 bg-hero bg-hero--3 h-44 sm:h-84">
-                    <!-- Title & Button -->
-                    <div class="flex flex-col items-center gap-y-4 md:gap-y-10 pt-5.25 md:pt-7">
-                        <h1 class="font-Morabba-Bold text-Tint-1 lg:text-[40px] text-base sm:text-2xl">تجربه
-                            غذایی به سبک گیاه خواران</h1>
-                        <button class="flex items-center h-6 px-2 text-sm text-white rounded-lg bg-Primary sm:px-4 lg:px-8 sm:h-8 lg:h-10 font-Dana sm:text-base lg:text-lg">
-                            <a href="#">سفارش آنلاین عذا</a>
-                        </button>
-                    </div>
-                </div>
+                <HeroSectionBanner bgNumber="3" content="تجربه غذای گیاهی به سبک ترخینه"/>
             </SwiperSlide>
             <SwiperSlide>
-                <div class="relative flex items-center justify-center bg-green-800 bg-hero bg-hero--4 h-44 sm:h-84">
-                    <!-- Title & Button -->
-                    <div class="flex flex-col items-center gap-y-4 md:gap-y-10 pt-5.25 md:pt-7">
-                        <h1 class="font-Morabba-Bold text-Tint-1 lg:text-[40px] text-base sm:text-2xl">تجربه
-                            نوشیدنی های گیاهی از دل طبیعت</h1>
-                        <button class="flex items-center h-6 px-2 text-sm text-white rounded-lg bg-Primary sm:px-4 lg:px-8 sm:h-8 lg:h-10 font-Dana sm:text-base lg:text-lg">
-                            <a href="#">سفارش آنلاین عذا</a>
-                        </button>
-                    </div>
-                </div>
+                <HeroSectionBanner bgNumber="4" content="نوشیدنی های گرم و سرد به سبک ترخینه"/>
             </SwiperSlide>
 
             <!-- Slider Pagination -->
