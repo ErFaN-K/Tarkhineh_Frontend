a0d01cd459d91ebe027db222edc0d51d85900cde
ErFaN-K <erfangamer138787@gmail.com>
2025-03-30 15:29:43 +0330

    Fix Hamburger Menu

--- src/components/HamburgerMenu.vue
+++ src/components/HamburgerMenu.vue
@@ -1,6 +1,12 @@
 <script setup>
+import { ref } from 'vue';
 import { usePublicStore } from '../stores/publicStore';
+
 const publicStore = usePublicStore()
+const activeSection = ref(null)
+const toggleSection = (sectionName) => {
+    activeSection.value = (activeSection.value === sectionName) ? null : sectionName;
+}
 </script>
 
 <template>
@@ -28,31 +34,49 @@ const publicStore = usePublicStore()
             </svg>
             <span class="text-xs font-Dana">صفحه اصلی</span>
         </div>
-        <div class="flex items-center justify-between">
-            <div class="flex items-center gap-x-1">
-                <svg class="size-4">
-                    <use href="#calender"></use>
-                </svg>
-                <span class="text-xs font-Dana">منو</span>
+        <div class="flex flex-col gap-y-2">
+            <div class="flex items-center justify-between" @click="toggleSection('menu')">
+                <div class="flex items-center gap-x-1">
+                    <svg class="size-4">
+                        <use href="#home-modern"></use>
+                    </svg>
+                    <span class="text-xs font-Dana">منو</span>
+                </div>
+                <span>
+                    <svg class="size-4">
+                        <use href="#chevron-down-mini"></use>
+                    </svg>
+                </span>
             </div>
-            <span>
-                <svg class="size-4">
-                    <use href="#chevron-down-mini"></use>
-                </svg>
-            </span>
+            <!-- DropDown -->
+            <div v-show="activeSection === 'menu'" class="pr-4 w-full *:text-gray-800 *:text-custom *:font-Dana *:block space-y-2">
+                <a href="#">منوی تستی</a>
+                <a href="#">منوی تستی</a>
+                <a href="#">منوی تستی</a>
+                <a href="#">منوی تستی</a>
+            </div>
         </div>
-        <div class="flex items-center justify-between">
-            <div class="flex items-center gap-x-1">
-                <svg class="size-4">
-                    <use href="#home-modern"></use>
-                </svg>
-                <span class="text-xs font-Dana">شعبه</span>
+        <div class="flex flex-col gap-y-2">
+            <div class="flex items-center justify-between" @click="toggleSection('branch')">
+                <div class="flex items-center gap-x-1">
+                    <svg class="size-4">
+                        <use href="#home-modern"></use>
+                    </svg>
+                    <span class="text-xs font-Dana">شعبه</span>
+                </div>
+                <span>
+                    <svg class="size-4">
+                        <use href="#chevron-down-mini"></use>
+                    </svg>
+                </span>
             </div>
-            <span>
-                <svg class="size-4">
-                    <use href="#chevron-down-mini"></use>
-                </svg>
-            </span>
+            <!-- DropDown -->
+            <div v-show="activeSection === 'branch'" class="pr-4 w-full *:text-gray-800 *:text-custom *:font-Dana *:block space-y-2">
+                <a href="#">شعبه اکباتان</a>
+                <a href="#">شعبه چالوس</a>
+                <a href="#">شعبه شهرک غرب</a>
+                <a href="#">شعبه تستی</a>
+            </div>
         </div>
         <div class="flex items-center gap-x-1">
             <svg class="size-4">
