5e464033a47966ad0a2e1e617003c6adfe209e96
ErFaN-K <erfangamer138787@gmail.com>
2025-04-01 12:10:59 +0330

    Fix a Tag in All Section

--- src/components/PagesComponent/HomePage/BranchCard.vue
+++ src/components/PagesComponent/HomePage/BranchCard.vue
@@ -21,19 +21,20 @@ defineProps({
 
 <template>
   <div class="flex flex-row w-full delay-100 border border-gray-400 rounded-lg md:flex-col hover:border-Primary">
-        <img :src="branchImage" class="max-md:min-w-36 max-md:max-h-20">
+        <a :href="branchPageLink">
+            <img :src="branchImage" class="max-md:min-w-36 max-md:max-h-20">
+        </a>
         <div class="flex flex-col items-center w-full p-2 text-center md:px-2 md:py-4 gap-y-1">
             <h3 class="text-sm text-gray-800 md:text-xl font-Morabba-Bold">{{ branchName }}</h3>
             <p
                 class="text-gray-700 text-custom md:text-sm font-Dana line-clamp-2 h-8 md:h-10 md:mb-2">
                 {{ branchAddress }}</p>
-            <button
-                class="flex items-center justify-center w-32 h-8 border rounded-sm gap-x-1 max-md:hidden border-Primary text-Primary">
-                <a :href="branchPageLink" class="text-xs font-Dana">صفحه شعبه</a>
+                <a :href="branchPageLink" class="text-xs font-Dana flex items-center justify-center w-32 h-8 border rounded-sm gap-x-1 max-md:hidden border-Primary text-Primary">
+                <span>صفحه شعبه</span>
                 <svg class="size-4">
                     <use href="#chevron-left-mini"></use>
                 </svg>
-            </button>
+            </a>
         </div>
     </div>
 </template>
