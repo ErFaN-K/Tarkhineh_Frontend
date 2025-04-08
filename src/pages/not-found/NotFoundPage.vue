589c81cdb81f1a776b458d9836642587ce77437d
ErFaN-K <erfangamer138787@gmail.com>
2025-04-08 19:55:45 +0330

    Fix Redirect Button Border Radius In Md Size

--- src/pages/not-found/NotFoundPage.vue
+++ src/pages/not-found/NotFoundPage.vue
@@ -8,8 +8,8 @@ import NotFound from '@/components/shared/404Error.vue'
     <NotFound/>
     <!-- Redirect Buttons -->
     <div class="flex max-md:flex-col w-full md:gap-x-4 max-md:gap-y-2 justify-center">
-        <a href="#" class="md:h-10 h-8 flex items-center justify-center md:max-w-45 w-full rounded-sm font-Dana text-xs text-Primary md:text-base bg-white border border-Primary hover:bg-Primary/10">بازگشت</a>
-        <a href="#" class="md:h-10 h-8 flex items-center justify-center md:max-w-45 w-full rounded-sm font-Dana text-white text-xs md:text-base bg-Primary hover:bg-Primary/90">صفحه اصلی</a>
+        <a href="#" class="md:h-10 h-8 flex items-center justify-center md:max-w-45 w-full rounded-sm md:rounded-lg font-Dana text-xs text-Primary md:text-base bg-white border border-Primary hover:bg-Primary/10">بازگشت</a>
+        <a href="#" class="md:h-10 h-8 flex items-center justify-center md:max-w-45 w-full rounded-sm md:rounded-lg font-Dana text-white text-xs md:text-base bg-Primary hover:bg-Primary/90">صفحه اصلی</a>
     </div>
   </div>
 </template>
