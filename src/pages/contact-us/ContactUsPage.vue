15b60e5220e758d999f3b2776bccf15d00a6a4cf
ErFaN-K <erfangamer138787@gmail.com>
2025-04-13 00:22:11 +0330

    Coding Contact Us Page & Its Components

--- src/pages/contact-us/ContactUsPage.vue
+++ src/pages/contact-us/ContactUsPage.vue
@@ -2,6 +2,11 @@
 import Header from '@/components/layout/SiteHeader.vue'
 import HeroSection from '@/components/ui/HeroSectionWithoutSlider.vue'
 import BranchSection from './components/BranchSection.vue'
+import Footer from '@/components/layout/SiteFooter.vue'
+import MobileMenu from "@/components/ui/MobileMenu.vue";
+import LoginForm from "@/components/shared/OtpLogin.vue";
+import SearchBoxModal from "@/components/shared/SearchBoxModal.vue";
+import Overlay from "@/components/ui/Overlay.vue";
 </script>
 
 <template>
@@ -17,4 +22,18 @@ import BranchSection from './components/BranchSection.vue'
         <!-- Contact Us | Branch Section -->
         <BranchSection />
     </main>
+
+    <!-- Contact Us | FOoter -->
+    <Footer />
+
+        <!----------------------->
+
+    <!-- Hamburger Menu -->
+    <MobileMenu />
+    <!-- Login Form -->
+    <LoginForm />
+    <!-- Search Box Modal ( For Desktop ) -->
+    <SearchBoxModal />
+    <!-- Overlay -->
+    <Overlay />
 </template>
