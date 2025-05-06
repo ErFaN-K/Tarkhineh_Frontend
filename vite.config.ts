0e2b8b3f703feebace896dd7f5bab19b6d240b5e
ErFaN-K <erfangamer138787@gmail.com>
2025-05-06 20:08:06 +0330

    Finish Project | Finish Front | Start Backend

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   resolve: {
